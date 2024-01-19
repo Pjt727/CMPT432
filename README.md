2ab166a3f83acb54b2913385c13f65a16997f2b0
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-01-19 17:12:02 -0500

    Lab 0: Development & Tooling
    Provided examples of using the rust cli tools to run and test code.

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ The three main bullets from that execeprt of that link are:
 - Rust comes with a inbuilt CLI tool, cargo, which is used to compile and run rust code.
 1. [Install](https://www.rust-lang.org/tools/install) rust for your system
 2. `cargo run` in the root directory. This compiles, if needed, and runs the program (See [here](https://doc.rust-lang.org/book/ch01-03-hello-cargo.html#building-and-running-a-cargo-project) for more info)
+- Depending on the assignment it may be better practice to use `cargo test` instead
+- In the example fibonacci code (may only be in one of the first commits), I show an example running versus testing the code base.
+- Using the correct cli tool provides code organization for free as test code is not even compiled in normal runs.
+- So for the assignments I will likely provide the example commands
+![Fib Test](assets/fib-test.png)
+![Fib Run](assets/fib-run.png)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+// Rust cached library to help with caching the function
+use cached::proc_macro::cached;
+
+// Newer mathematical definition of the fib sequence where the first is 0
+#[cached]
+fn fib_sequence(index: u32) -> u32 {
+    if index == 0 { return 0; }
+    if index == 1 || index == 2 { return 1; }
+    return fib_sequence(index - 1) + fib_sequence(index - 2);
 }
+
+#[test]
+fn test_fibs() {
+    let fib_0: u32 = 0;
+    let fib_1: u32 = 1;
+    let fib_2: u32 = 1;
+    let fib_3: u32 = 2;
+    let fib_4: u32 = 3;
+    let fib_5: u32 = 5;
+    let fib_6: u32 = 8;
+    let fib_7: u32 = 13;
+    let fib_8: u32 = 21;
+    let fib_9: u32 = 34;
+    let fib_10: u32 = 55;
+
+    assert_eq!(fib_0, fib_sequence(0), "Testing if fib index {} evaluates to {}", 0, fib_0);
+    assert_eq!(fib_1, fib_sequence(1), "Testing if fib index {} evaluates to {}", 1, fib_1);
+    assert_eq!(fib_2, fib_sequence(2), "Testing if fib index {} evaluates to {}", 2, fib_2);
+    assert_eq!(fib_3, fib_sequence(3), "Testing if fib index {} evaluates to {}", 3, fib_3);
+    assert_eq!(fib_4, fib_sequence(4), "Testing if fib index {} evaluates to {}", 4, fib_4);
+    assert_eq!(fib_5, fib_sequence(5), "Testing if fib index {} evaluates to {}", 5, fib_5);
+    assert_eq!(fib_6, fib_sequence(6), "Testing if fib index {} evaluates to {}", 6, fib_6);
+    assert_eq!(fib_7, fib_sequence(7), "Testing if fib index {} evaluates to {}", 7, fib_7);
+    assert_eq!(fib_8, fib_sequence(8), "Testing if fib index {} evaluates to {}", 8, fib_8);
+    assert_eq!(fib_9, fib_sequence(9), "Testing if fib index {} evaluates to {}", 9, fib_9);
+    assert_eq!(fib_10, fib_sequence(10), "Testing if fib index {} evaluates to {}", 10, fib_10);
+}
+
+fn main() {
+    // the first arg is the target
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 { panic!("\nExpected an index for the Fibonacci sequence!\n") }
+    if args.len() > 2 { panic!("\nOnly one index can be computed at a time!\n") }
+
+    let fib_index_input = &args[1];
+    let fib_index = fib_index_input.parse::<u32>();
+    match fib_index {
+        Ok(n) => {
+            println!("The Fibonacci sequence at index {} is {}", n, fib_sequence(n))
+        }
+        Err(e) => panic!("\nThere was an error parsing your whole number: {}\n", e)
+    }
+}
+
