17dc258f53b06e580826a544d1203706c0e5ad93
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-01-26 16:45:23 -0500

    Project 1: Started on cli
    Started the cli to pass .txt files to the compiler with error checking.
    Also incorporated the idea of passing a directory to compile all files
    within that directory

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,37 @@
+use std::path::Path;
 use std::env;
-// Rust cached library to help with caching the function
-use cached::proc_macro::cached;
+use std::fs;
 
-// Newer mathematical definition of the fib sequence where the first is 0
-#[cached]
-fn fib_sequence(index: u32) -> u32 {
-    if index == 0 { return 0; }
-    if index == 1 || index == 2 { return 1; }
-    return fib_sequence(index - 1) + fib_sequence(index - 2);
-}
-
-#[test]
-fn test_fibs() {
-    let fib_0: u32 = 0;
-    let fib_1: u32 = 1;
-    let fib_2: u32 = 1;
-    let fib_3: u32 = 2;
-    let fib_4: u32 = 3;
-    let fib_5: u32 = 5;
-    let fib_6: u32 = 8;
-    let fib_7: u32 = 13;
-    let fib_8: u32 = 21;
-    let fib_9: u32 = 34;
-    let fib_10: u32 = 55;
-
-    assert_eq!(fib_0, fib_sequence(0), "Testing if fib index {} evaluates to {}", 0, fib_0);
-    assert_eq!(fib_1, fib_sequence(1), "Testing if fib index {} evaluates to {}", 1, fib_1);
-    assert_eq!(fib_2, fib_sequence(2), "Testing if fib index {} evaluates to {}", 2, fib_2);
-    assert_eq!(fib_3, fib_sequence(3), "Testing if fib index {} evaluates to {}", 3, fib_3);
-    assert_eq!(fib_4, fib_sequence(4), "Testing if fib index {} evaluates to {}", 4, fib_4);
-    assert_eq!(fib_5, fib_sequence(5), "Testing if fib index {} evaluates to {}", 5, fib_5);
-    assert_eq!(fib_6, fib_sequence(6), "Testing if fib index {} evaluates to {}", 6, fib_6);
-    assert_eq!(fib_7, fib_sequence(7), "Testing if fib index {} evaluates to {}", 7, fib_7);
-    assert_eq!(fib_8, fib_sequence(8), "Testing if fib index {} evaluates to {}", 8, fib_8);
-    assert_eq!(fib_9, fib_sequence(9), "Testing if fib index {} evaluates to {}", 9, fib_9);
-    assert_eq!(fib_10, fib_sequence(10), "Testing if fib index {} evaluates to {}", 10, fib_10);
-}
 
 fn main() {
     // the first arg is the target
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1 { panic!("\nExpected an index for the Fibonacci sequence!\n") }
-    if args.len() > 2 { panic!("\nOnly one index can be computed at a time!\n") }
-
-    let fib_index_input = &args[1];
-    let fib_index = fib_index_input.parse::<u32>();
-    match fib_index {
-        Ok(n) => {
-            println!("The Fibonacci sequence at index {} is {}", n, fib_sequence(n))
+    if args.len() == 1 { panic!("\nExpected files or directory input!\n") }
+    let first_path = Path::new(&args[1]);
+    if !first_path.exists() {
+        panic!("\nThat path does not exist! Please place files / folders in the root directory.\n")
+    } else if first_path.is_dir() {
+        let files = fs::read_dir(first_path).unwrap();
+        for file in files {
+            let file = match file {
+                Ok(file) => file,
+                Err(_) => continue,
+            };
+            let file_path = file.path();
+            match file_path.extension() {
+                Some(extension) => 
+                    if extension != ".txt" { continue; },
+                None => continue 
+            }
+            // TODO LEX file_path
+            // file_path.as_ref()
         }
-        Err(e) => panic!("\nThere was an error parsing your whole number: {}\n", e)
+    } else if let Some(extension) = first_path.extension() {
+        if (extension != ".txt") {
+            panic!("That is not a valid filetype to compile!")
+        }
+        // TODO LEX file_path
     }
 }
+
 
