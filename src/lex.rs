2d0a64d3d207f097eaf3330d4d1b5760fcd3c597
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-01-29 16:01:10 -0500

    Project 1: Match Statements

--- src/lex.rs
+++ src/lex.rs
@@ -14,6 +14,8 @@ use std::{
 enum Keyword {
     LoopOnTrue,
     If,
+    Boolean,
+    Int,
     True,
     False,
     Print,
@@ -44,11 +46,65 @@ struct Char {
     letter: char,
 }
 
-struct Token {}
+enum TokenKind {
+    Keyword(Keyword),
+    Id(Id),
+    Symbol(Symbol),
+    Digit(Digit),
+    Char(Char),
+}
 
-fn get_token(buffer: &String, in_string: bool) -> Option<Token> {
+struct Token {
+    kind: TokenKind,
+}
+
+// non char, id, and number range are single char that can use regex to
+// or other range techniques to discover
+// It also seems pointless to use regex here
+fn get_non_range_token(buffer: &str) -> Option<Token> {
     match buffer {
-        // MY OTHER CONDITIONS
+        "boolean" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::Boolean),
+        }),
+        "while" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::LoopOnTrue),
+        }),
+        "print" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::Print),
+        }),
+        "false" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::False),
+        }),
+        "true" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::True),
+        }),
+        "int" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::Int),
+        }),
+        "if" => Some(Token {
+            kind: TokenKind::Keyword(Keyword::If),
+        }),
+        "==" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::CheckEqaulity),
+        }),
+        "!=" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::CheckIneqaulity),
+        }),
+        "=" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::Assignment),
+        }),
+        "(" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::OpenParenthesis),
+        }),
+        ")" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::CloseParenthesis),
+        }),
+        "{" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::OpenBlock),
+        }),
+        "}" => Some(Token {
+            kind: TokenKind::Symbol(Symbol::CloseBlock),
+        }),
         _ => None,
     }
 }
@@ -61,6 +117,8 @@ fn lex_file(path: &Path) -> Option<TokenStream> {
     let file = File::open(path).expect(&format!("Failed to open file, {}", path.to_string_lossy()));
     let reader = BufReader::new(file);
 
+    // I am pretty sure it would be faster to just have
+    // a set of all chars but we gotta use regex for something
     let in_alphabet: Regex = Regex::new("a-z0-9{}[]!=").unwrap();
     let mut line_number = 1;
     let mut current_char_position = 0;
