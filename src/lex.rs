b151193449f3c366c579058c2d51ab11b73147bd
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-01-31 15:23:42 -0500

    Project 1: Skeleton for lex looping
    Wrote the skeleton for how lex will loop through the files.
    Completed everything but the fold method.

--- src/lex.rs
+++ src/lex.rs
@@ -113,31 +113,63 @@ struct TokenStream {
     next_token: Box<Option<TokenStream>>,
 }
 
+fn fold(token_stream: TokenStream, start_position: i32) {}
+
 fn lex_file(path: &Path) -> Option<TokenStream> {
     let file = File::open(path).expect(&format!("Failed to open file, {}", path.to_string_lossy()));
     let reader = BufReader::new(file);
 
-    // I am pretty sure it would be faster to just have
-    // a set of all chars but we gotta use regex for something
+    const SYMBOL_MAX_SIZE: i32 = 2;
+
+    // we gotta use regex for something
     let in_alphabet: Regex = Regex::new("a-z0-9{}[]!=").unwrap();
+    let mut in_string = false;
+    let mut last_is_symbol = false;
     let mut line_number = 1;
+    // set to negative to propely increment at start 0 since we preincrement
+    let mut start_char_position = -1;
     let mut current_char_position = 0;
     let mut buffer = String::from("");
     let token_stream = Default::default();
     for line in reader.lines() {
+        // poor preformance for insanely long single line files
+        // can choose other deliminators
         let line = line.expect("Unexpected File Reading Error");
         for c in line.chars() {
             current_char_position += 1;
             // Character processing
-            if c.is_ascii_lowercase() {
-                buffer.push(c);
-                continue;
+            if (c >= 'a' && c <= 'z') && !last_is_symbol {
+                // do nothing because character followed by character
+            } else if c == '"' {
+                // fold
+                in_string = !in_string;
+                // Hey! isn't this technically a bit of parsing?
+                // ----- open satire -----
+                // Where do we draw the line?
+                // How far do we go?
+                // Do we return full string token?
+                // Do we return full blocks together with recursive types?
+                // Do we return just return the full CST?
+                // Do we just return the AST?
+                // Do we just return the generated code?
+                // Do we just execute the result of the program and return that?
+                // Do we just sense the intent of the programmer by reading their mind
+                //  and return the output?
+                // ----- close satire -----
+
+                // if buffer is currently string then fold bc it reached symbol
+                //    or if symbol litmit has exceded
+            } else if !last_is_symbol
+                || ((start_char_position - current_char_position) > SYMBOL_MAX_SIZE)
+            {
+                // fold
             }
-            new_token = get_token(&buffer, true);
             buffer.push(c);
         }
         line_number += 1;
         current_char_position = 0;
     }
+    // Need to fold for end of file
+    // fold
     return token_stream;
 }
