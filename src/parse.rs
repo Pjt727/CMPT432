49038ec8b6d99e3acec7d329429dc01f1e69d884
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-03-08 17:16:20 -0500

    Project 2: Oh I guess it just works (maybe)
    Changed the type system to make to enforce the correct types instead of
    relying on runtime enum checks

--- src/parse.rs
+++ src/parse.rs
@@ -1,15 +1,15 @@
+
 use std::{cell::RefCell, rc::Rc, rc::Weak};
 
-use crate::token::*;
-
-type Node<'a> = Rc<RefCell<NodeEnum<'a>>>;
+mod token;
+use token::*;
 
 struct Production<'a> {
     // chosen as string instead of a enum because
     // every produciton rule is only added in one place
     rule: String,
-    children: Vec<Node<'a>>,
-    parent: Option<Weak<RefCell<NodeEnum<'a>>>>,
+    children: Vec<NodeEnum<'a>>,
+    parent: Option<Weak<RefCell<Production<'a>>>>,
 }
 
 struct ParseError<'a> {
@@ -18,44 +18,19 @@ struct ParseError<'a> {
 }
 
 enum NodeEnum<'a> {
-    Production(Production<'a>),
+    Production(Rc<RefCell<Production<'a>>>),
     Terminal(&'a Token),
-}
-
-// becauase of the design it is difficult to get the enum type out of the node
-// to do the adding so this sort of awkward code arisen where I do the matching in
-// this method
-impl<'a> NodeEnum<'a> {
-    fn add_terminal(&mut self, node: &'a Token) {
-        let node = Rc::new(RefCell::new(NodeEnum::Terminal(node)));
-        match self {
-            NodeEnum::Production(p) => {
-                p.children.push(node);
-            }
-            NodeEnum::Terminal(_) => panic!("Cannot add to a terminal"),
-        }
-    }
-
-    fn add_production(&mut self, new_production: Node<'a>) {
-        match self {
-            NodeEnum::Production(p) => {
-                p.children.push(new_production);
-            }
-            NodeEnum::Terminal(_) => panic!("Cannot add to a terminal"),
-        }
-    }
 }
 
 struct ConcreteSyntaxTree<'a, T>
 where
     T: Iterator<Item = &'a Token>,
 {
-    root: Result<Node<'a>, ParseError<'a>>,
+    root: Result<Rc<RefCell<Production<'a>>>, ParseError<'a>>,
     tokens: T,
-
     // this is only ever supposed to be a production
     // but it would be difficult to change types becuase nodes reference counted
-    last_node: Weak<RefCell<NodeEnum<'a>>>,
+    last_production: Weak<RefCell<Production<'a>>>,
 }
 
 impl<'a, T> ConcreteSyntaxTree<'a, T>
@@ -63,15 +38,15 @@ where
     T: Iterator<Item = &'a Token>,
 {
     pub fn new(tokens: T) -> Self {
-        let root_node = Rc::new(RefCell::new(NodeEnum::Production(Production {
+        let root_node = Rc::new(RefCell::new(Production {
             rule: "program".to_string(),
             children: vec![],
             parent: None,
-        })));
+        }));
         let mut cst = ConcreteSyntaxTree {
             root: Ok(root_node.clone()),
             tokens,
-            last_node: Rc::downgrade(&root_node),
+            last_production: Rc::downgrade(&root_node),
         };
         cst.do_program();
         return cst;
@@ -79,35 +54,35 @@ where
 
     // moves the last_node to the parrent of the current node
     fn up_root(&mut self) {
-        let last_node_weak = &self.last_node;
-        let last_node = last_node_weak.upgrade().unwrap();
-        let last_node_strong = last_node.borrow_mut();
-        let node_enum = &*last_node_strong;
-        match node_enum {
-            NodeEnum::Production(_) => todo!(),
-            NodeEnum::Terminal(_) => todo!(),
+        let last_production_weak = &self.last_production;
+        let last_production_strong = last_production_weak.upgrade().unwrap();
+        let last_production = last_production_strong.borrow_mut();
+        match &last_production.parent {
+            Some(parent) => self.last_production = parent.clone(),
+            None => panic!("cannot uproot when there is no parent"),
         }
     }
 
     // has the side affect of moving the last_node to the added production
     fn add_production(&mut self, production_name: String) {
-        let last_node_weak = &self.last_node;
-        let last_node = last_node_weak.upgrade().unwrap();
-        let mut last_node_strong = last_node.borrow_mut();
-        let node = Rc::new(RefCell::new(NodeEnum::Production(Production {
+        let last_production_weak = &self.last_production;
+        let last_production_strong = last_production_weak.upgrade().unwrap();
+        let mut last_node = last_production_strong.borrow_mut();
+        let new_production = Rc::new(RefCell::new(Production {
             rule: production_name,
             children: vec![],
-            parent: Some(last_node_weak.clone()),
-        })));
-        self.last_node = Rc::downgrade(&node);
-        last_node_strong.add_production(node);
+            parent: Some(last_production_weak.clone()),
+        }));
+        self.last_production = Rc::downgrade(&new_production);
+        let node = NodeEnum::Production(new_production);
+        last_node.children.push(node);
     }
 
     // always consumes token
     fn match_kind(&mut self, kinds: Vec<TokenKind>) {
-        let last_node_weak = &self.last_node;
-        let last_node = last_node_weak.upgrade().unwrap();
-        let mut last_node_strong = last_node.borrow_mut();
+        let last_production_weak = &self.last_production;
+        let last_production_strong = last_production_weak.upgrade().unwrap();
+        let mut last_node = last_production_strong.borrow_mut();
         let token = self.tokens.next();
         match token {
             Some(t) => {
@@ -115,11 +90,12 @@ where
                     // will need to change this to ensure that it properly matches
                     // the nested types
                     if matches!(&t.kind, kind) {
-                        last_node_strong.add_terminal(t);
+                        last_node.children.push(NodeEnum::Terminal(&t));
                         return;
                     }
                 }
-                return;
+                // will change to error later
+                panic!("Did not match any tokens");
             }
 
             // maybe change this panic to an error message
@@ -139,3 +115,6 @@ where
 {
     let cst = ConcreteSyntaxTree::new(tokens);
 }
+
+
+
