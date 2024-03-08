9b849e63bbe2ee1249f42031afb5f0e6e3bf4145
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-03-08 12:20:16 -0500

    Project 2: Close to a tree solution
    I spent some time using a simpler example in a different repository to
    try to understand the how reference counters and other things worked in
    Rust. I think this is close to a solution that will work in rust

--- src/parse.rs
+++ src/parse.rs
@@ -1,18 +1,22 @@
+use std::{cell::RefCell, rc::Rc, rc::Weak};
+
 use crate::token::*;
 
-struct Produdction<'a> { 
+struct Production<'a> {
     rule: String,
-    children: Vec<Node<'a>>
+    children: Vec<Node<'a>>,
 }
 
 struct ParseError<'a> {
     token_found: Option<&'a Token>,
-    expected_kinds: Vec<TokenKind>
+    expected_kinds: Vec<TokenKind>,
 }
 
-enum Node<'a> {
-    Production(Produdction<'a>),
-    Terminal(&'a Token)
+type Node<'a> = Rc<RefCell<NodeEnum<'a>>>;
+
+enum NodeEnum<'a> {
+    Production(Production<'a>),
+    Terminal(&'a Token),
 }
 
 struct ConcreteSyntaxTree<'a, T>
@@ -21,6 +25,7 @@ where
 {
     root: Result<Node<'a>, ParseError<'a>>,
     tokens: T,
+    last_node: Weak<RefCell<NodeEnum<'a>>>,
 }
 
 impl<'a, T> ConcreteSyntaxTree<'a, T>
@@ -28,13 +33,14 @@ where
     T: Iterator<Item = &'a Token>,
 {
     pub fn new(tokens: T) -> Self {
-        let root_node = Node::Production(Produdction { 
-                rule: "root".to_string(), 
-                children: vec![]
-            });
-        let mut cst = ConcreteSyntaxTree { 
-            root: Ok(root_node),
-            tokens
+        let root_node = Rc::new(RefCell::new(NodeEnum::Production(Production {
+            rule: "program".to_string(),
+            children: vec![],
+        })));
+        let mut cst = ConcreteSyntaxTree {
+            root: Ok(root_node.clone()),
+            tokens,
+            last_node: Rc::downgrade(&root_node),
         };
         cst.do_program();
         return cst;
@@ -42,16 +48,7 @@ where
 
     // always consumes token
     fn match_kind(&mut self, kinds: Vec<TokenKind>) {
-        let current_production = match &mut self.root {
-            Ok(n) => {
-                match n {
-                    Node::Production(p) => p,
-                    // should never be not a production
-                    _ => panic!("Tried adding a non terminal to a non terminal?"),
-                }
-            },
-            Err(_) => return
-        };
+        let last_node_strong = &mut self.last_node.upgrade().unwrap();
         let token = self.tokens.next();
         match token {
             Some(t) => {
@@ -60,13 +57,13 @@ where
                     // the nested types
                     if matches!(&t.kind, kind) {
                         let node = Node::Terminal(&t);
-                        current_production.children.push(node);
+                        last_production.children.push(node);
                         return;
                     }
                 }
-                
+
                 return;
-            },
+            }
             None => return,
         }
     }
@@ -75,9 +72,7 @@ where
         // other stuff
         self.match_kind(vec![TokenKind::Symbol(Symbol::EndProgram)]);
     }
-
 }
-
 
 pub fn parse<'a, T>(mut tokens: T)
 where
@@ -85,4 +80,3 @@ where
 {
     let cst = ConcreteSyntaxTree::new(tokens);
 }
-
