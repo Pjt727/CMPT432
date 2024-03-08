81100a80545867a48fa65fcb01294a730a41c6f6
Patrick <76497944+Pjt727@users.noreply.github.com>
2024-03-08 15:14:07 -0500

    Project 2: Well not compielr errors rn
    Built the match_kind function with out compile errors. It feels kind of
    difficult to write memory safe trees in rust

--- src/parse.rs
+++ src/parse.rs
@@ -2,6 +2,8 @@ use std::{cell::RefCell, rc::Rc, rc::Weak};
 
 use crate::token::*;
 
+type Node<'a> = Rc<RefCell<NodeEnum<'a>>>;
+
 struct Production<'a> {
     rule: String,
     children: Vec<Node<'a>>,
@@ -12,11 +14,24 @@ struct ParseError<'a> {
     expected_kinds: Vec<TokenKind>,
 }
 
-type Node<'a> = Rc<RefCell<NodeEnum<'a>>>;
-
 enum NodeEnum<'a> {
     Production(Production<'a>),
     Terminal(&'a Token),
+}
+
+impl<'a> NodeEnum<'a> {
+    // it might be better to have this method be on the
+    // the production type but it is sort of difficult
+    // to get the reference production from a node enum ref count
+    fn add_terminal(&mut self, node: &'a Token) {
+        let node = Rc::new(RefCell::new(NodeEnum::Terminal(node)));
+        match self {
+            NodeEnum::Production(p) => {
+                p.children.push(node);
+            }
+            NodeEnum::Terminal(_) => panic!("Cannot add to a terminal"),
+        }
+    }
 }
 
 struct ConcreteSyntaxTree<'a, T>
@@ -46,9 +61,12 @@ where
         return cst;
     }
 
+    fn add_prodction(&mut self, production_name: String) {}
     // always consumes token
     fn match_kind(&mut self, kinds: Vec<TokenKind>) {
-        let last_node_strong = &mut self.last_node.upgrade().unwrap();
+        let last_node_weak = &self.last_node;
+        let last_node = last_node_weak.upgrade().unwrap();
+        let mut last_node_strong = last_node.borrow_mut();
         let token = self.tokens.next();
         match token {
             Some(t) => {
@@ -56,15 +74,15 @@ where
                     // will need to change this to ensure that it properly matches
                     // the nested types
                     if matches!(&t.kind, kind) {
-                        let node = Node::Terminal(&t);
-                        last_production.children.push(node);
+                        last_node_strong.add_terminal(t);
                         return;
                     }
                 }
-
                 return;
             }
-            None => return,
+
+            // maybe change this panic to a error message
+            None => panic!("Ran out of tokens"),
         }
     }
 
